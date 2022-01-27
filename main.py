ba41aec5f5ab5e0658a31a4b1cd789074e2f3dcb
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-27 16:25:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+Given an array of integer as input, print true if two even numbers and two odd numbers appear next to each other in the array.
+Input-> [1,2,3,9,4,5,6]
+Output-> 2Odd+2Even : false
+"""
+
